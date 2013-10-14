4d5ea0d871be0d91ec28c6da068a0e5c49b87d63
John McLear <john@mclear.co.uk>
2013-10-14 15:53:25 +0100

    computer fix

--- static/js/disable_error_messages.js
+++ static/js/disable_error_messages.js
@@ -4,7 +4,7 @@ exports.suppress = function (hook_name, args, cb) {
   setTimeout(function(){
     $("#editorloadingbox").hide("Error messages suppressed by your system administrator");
     $("#editorcontainerbox").append("<div id='suppresserror' style='text-align:center;margin-top:50px;'><h1>Error messages suppressed by your system administrator</h1></div>");
-  }, 500);
+  }, 1000);
 }
 exports.postAceInit = function (hook_name, args, cb) {
   $("#suppresserror").hide();
